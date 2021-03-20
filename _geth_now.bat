7c7cd48a416d3bee59fcc6770f217e92c8e16a33
sooooz <soojinpar1026@gmail.com>
2021-03-21 01:58:00 +0900

    _geth_now.bat

---
+++ _geth_now.bat
@@ -0,0 +1,1 @@
+geth --identity "psj" --allow-insecure-unlock --datadir .\eth --rpc --rpcaddr "localhost" --rpcport "8445" --rpccorsdomain "*" --port "38445" --rpcapi "admin, db, eth, debug, miner, net, shh, txpool, personal, web3" --networkid 33

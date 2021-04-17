1696e73642d7e2f18c26fa56084bc4265fd06106
sooziini <soojinpar1026@gmail.com>
2021-04-18 01:13:15 +0900

    e7-contract-hello

--- src/e7-contract-helloUse.js
+++ src/e7-contract-helloUse.js
@@ -2,5 +2,5 @@ var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8345"));
 
 var shelloContract = new web3.eth.Contract([{"inputs":[],"name":"sayHello","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"}],
-                                          "0x8A870bbeb54F7Ba1f7775b0107dbF8e050276eB8");
+                                          "0xf32CB41EEF7F6d024e6136B07Bb3C8fad19354f7");
 shelloContract.methods.sayHello().call().then(function(str) {console.log(str);});

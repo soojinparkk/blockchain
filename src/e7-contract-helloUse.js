3d1dd9fbcddaea52867e1ecd031cc2f9c8654d5e
sooziini <soojinpar1026@gmail.com>
2021-04-22 00:34:20 +0900

    dApp-hello-ex (use contract-hello-ex.ipynb)

--- src/e7-contract-helloUse.js
+++ src/e7-contract-helloUse.js
@@ -2,5 +2,5 @@ var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8345"));
 
 var shelloContract = new web3.eth.Contract([{"inputs":[],"name":"sayHello","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"}],
-                                          "0xf32CB41EEF7F6d024e6136B07Bb3C8fad19354f7");
+                                          "0x3EC0C983f0A405940CdEEa44c77db4Da72A37e44");
 shelloContract.methods.sayHello().call().then(function(str) {console.log(str);});
